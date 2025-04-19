10b2f1c8dcca2ef2f65a2b08eddb0fcaabb622e1
Victor Pipin <parabletraveller@gmail.com>
2025-04-19 13:05:51 +0300

    Feat: Add routing to the application, Wrap the whole application in a <BrowserRouter> component and add routing for the application, homepage and product page

--- shop-page/src/App.tsx
+++ shop-page/src/App.tsx
@@ -1,11 +1,18 @@
+
+import { Route, Routes } from "react-router";
 import "./App.css";
 import { BestSellers } from "./BestSeller";
 import { Header } from "./Header";
+import { Product } from "./Product";
 
 function App() {
   return (
     <div className="appContainer">
       <Header/>
+      <Routes>
+        <Route path="/" element={ <BestSellers /> } />
+        <Route path="/product" element={ <Product /> } />
+      </Routes>
       <BestSellers/>
     </div>
   );
